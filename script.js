2b0081b67f9a7ce1986b762973d84d1bb3a46186
Jayanth J <153603039+imjayanth5556@users.noreply.github.com>
2024-01-29 17:31:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Download CV //
+$(document).ready(function(){
+  $("#btn").click(function()
+  {
+      window.location.href = "Jayanth.UI.UX.Designer.pdf";
+  });
+});
+
+$(document).ready(function(){
+  let typed = new Typed(".ui", {
+      strings: ['UI / UX Designer', 'Web Developer'],
+      typeSpeed: 125,
+      backSpeed: 75,
+      loop: true
+  });
+});
